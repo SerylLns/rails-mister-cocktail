9f8c9b912c5225d08797e18127f317c05d93f985
seryl lounis <seryllounis@outlook.fr>
2021-04-02 17:19:42 +0200

    nothing on Ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
-Review.destroy_all
-Cocktail.destroy_all
+# Review.destroy_all
+# Cocktail.destroy_all
 # # This file should contain all the record creation needed to seed the database with its default values.
 # # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 # #
 # require 'json'
 # require 'open-uri'
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# user_serialized = open(url).read
-# ingredient = JSON.parse(user_serialized)
-# #
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+ingredient = JSON.parse(user_serialized)
+#
 # #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 # #   Character.create(name: 'Luke', movie: movies.first)
 # Dose.destroy_all
 # Ingredient.destroy_all
 
-# ingredient["drinks"].each do |i|
-#   Ingredient.create!(name: i["strIngredient1"])
-# end
+ingredient["drinks"].each do |i|
+  Ingredient.create!(name: i["strIngredient1"])
+end
